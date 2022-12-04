4fc1c1b960da425781fa42d873ae9b9cddb4b215
Brittany <brittanydesktop@localhost>
2022-12-04 12:58:33 -0600

    Added code to Character class

--- Game/src/main/java/com/teamDecimal/game/Character.java
+++ Game/src/main/java/com/teamDecimal/game/Character.java
@@ -9,5 +9,41 @@ package com.teamDecimal.game;
  * @author brittanydesktop
  */
 public class Character {
+    /*Attributes*/
+    int currenthealth;
+    int startingHealth;
+    int attackDamage;
+    
+    /*Constructors*/
+    public int getAttackDamage(){
+        return attackDamage;
+    }
+    
+    public int getStartingHealth(){
+        return startingHealth;
+    }
+    
+    public void setStartingHealth(int startingHealth){
+        this.startingHealth = startingHealth;
+    }
+    
+    public void setCurrentHealth(int currentHealth){
+        this.currenthealth = currentHealth;
+    }
+    
+    public int getCurrentHealth(){
+        return currenthealth;
+    }
+    
+    /*Methods*/
+    public boolean isAlive(){
+        boolean isAlive;
+        if(currenthealth <= 0){
+            isAlive = false;
+        }else{
+            isAlive = true;
+        }
+        return isAlive;
+    }
     
 }
